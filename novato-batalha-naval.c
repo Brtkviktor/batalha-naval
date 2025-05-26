ddd40d8de8c07a81f4aaca345a6e2d7dfa85bd62
João Victor <joaoviktorbrkt@gmail.com>
2025-05-26 02:17:01 -0300

    atualizando a Legibilidade dos codigos

--- novato-batalha-naval.c
+++ novato-batalha-naval.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    
-    int tabuleiro[10][10] ={0}; // Tabuleiro 10x10 inicializado com 0 (água)
+int main()
+{
 
-    int navio = 3;       // Valor que representa o navio
-    int tam_navio = 3;   // Tamanho do navio
+    int tabuleiro[10][10] = {0}; // Tabuleiro 10x10 inicializado com 0 (água)
+
+    int navio = 3;     // Valor que representa o navio
+    int tam_navio = 3; // Tamanho do navio
 
     // Coordenadas iniciais dos navios
     int linha_horizontal = 2;
@@ -15,32 +16,36 @@ int main(){
     int coluna_vertical = 3;
 
     // Posiciona navio horizontal
-    for (int i = 0; i < tam_navio; i++) {
+    for (int i = 0; i < tam_navio; i++)
+    {
         tabuleiro[linha_horizontal][coluna_horizontal + i] = navio;
     }
 
     // Posiciona navio vertical
-    for (int i = 0; i < tam_navio; i++) {
+    for (int i = 0; i < tam_navio; i++)
+    {
         tabuleiro[linha_vertical + i][coluna_vertical] = navio;
     }
 
-    
     printf("## Tabuleiro Batalha Naval ##\n");
     // Exibe o cabeçalho com letras A-J
     printf("   ");
-    for (char letra = 'A'; letra <= 'J'; letra++) {
+    for (char letra = 'A'; letra <= 'J'; letra++)
+    {
         printf("%c ", letra);
     }
     printf("\n");
 
     // Exibe o tabuleiro
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++)
+    {
         printf("%2d ", i); // Número da linha
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < 10; j++)
+        {
             printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
     }
-    
+
     return 0;
 }
